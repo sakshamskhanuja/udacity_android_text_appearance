728c01bfb53bb96f9ac4578f01b34e358af4e410
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-03-09 22:08:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/basic/textappearance/MainActivity.java
@@ -0,0 +1,14 @@
+package com.basic.textappearance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
